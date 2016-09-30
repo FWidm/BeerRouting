23c3b1e26db72f25c4164d0994818024b9024cff
FWidm <fabian.widmann@uni-ulm.de>
2016-09-30 12:00:41 +0200

    Something went wrong in the last commit, trying to get all scripts into github

---
+++ BeerRouting/Assets/Scripts/Main/DisplayPathHovers.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayPathHovers : MonoBehaviour
+{
+    public bool show
+    {
+        get;
+        set;
+    }
+
+    public void ClickButtonHighlight()
+    {
+        show = !show;
+        Debug.Log("Button Highlight clicked!" + show);
+
+    }
+}
